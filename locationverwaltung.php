89a8d2509634e6e044b687631e93503042f6ff6b
Domihansa <dominik.widera@t-online.de>
2016-03-25 21:22:29 +0100

    Zwei kleine Fehler behoben
    Im insert war noch was doppelt, bei den locations auch noch ne
    Kleinigkeit falsch.

--- locationverwaltung.php
+++ locationverwaltung.php
@@ -1,6 +1,8 @@
 <?php
-require('includes/includeDatabase.php');
-
+session_start();
+if(!isset($_SESSION['userid'])) {
+	die('Bitte zuerst <a href="index.php">einloggen</a>');
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
