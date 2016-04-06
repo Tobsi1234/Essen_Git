d36d05dc85f186044b53f9b4339905de91be709f
Tobsi1234 <tobias.steinbrueck@web.de>
2016-04-06 19:42:19 +0200

    todo
    todo als csv statt xls datei damit man in github die änderungen sieht...
    csv dateien lassen sich aber auch mit excel öffnen/ändern

--- includes/includeDatabase.php
+++ includes/includeDatabase.php
@@ -2,11 +2,11 @@
 
 $sqlhost = "localhost";
 $sqluser = "root";
-$sqlpass = "";
+$sqlpass = "55775577";
 $connection = mysqli_connect($sqlhost, $sqluser, $sqlpass) or die ("DB-system nicht verfügbar");
 mysqli_select_db($connection, "tobsi") or die ("Datenbank nicht verfügbar");
 
-$pdo = new PDO('mysql:host=localhost;dbname=tobsi', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=tobsi', 'root', '55775577');
 
 
 ?>
