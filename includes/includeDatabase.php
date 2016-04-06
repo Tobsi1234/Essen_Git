e47d6b83bbb959ead0850329cafd068584969f40
Tobsi1234 <tobias.steinbrueck@web.de>
2016-04-06 19:44:56 +0200

    ausversehen includeDB mit upgedated

--- includes/includeDatabase.php
+++ includes/includeDatabase.php
@@ -2,11 +2,11 @@
 
 $sqlhost = "localhost";
 $sqluser = "root";
-$sqlpass = "55775577";
+$sqlpass = "";
 $connection = mysqli_connect($sqlhost, $sqluser, $sqlpass) or die ("DB-system nicht verfügbar");
 mysqli_select_db($connection, "tobsi") or die ("Datenbank nicht verfügbar");
 
-$pdo = new PDO('mysql:host=localhost;dbname=tobsi', 'root', '55775577');
+$pdo = new PDO('mysql:host=localhost;dbname=tobsi', 'root', '');
 
 
 ?>
