4441bc59ab77240be8dd622ec0cf9e3d359f0a7d
Tobsi1234 <tobias.steinbrueck@web.de>
2016-04-06 11:13:26 +0200

    javascript von locationsverwaltung in page_1 bzw. page_2

--- locationverwaltung.php
+++ locationverwaltung.php
@@ -12,122 +12,7 @@ require("includes/includeDatabase.php");
     include("includes/includeHead.php");
     // include("procedures.php");
     ?>
-
-
-    <script language="javascript">
-        <!--
-        var XMLreq, referenz, meinEssen, name, element, box, error, locname, locpage, essenName;
-        var locessen = [];
-        var essenDropdown = [];
-
-        function loc_anlegen() {
-            // alert("Ich lege eine Location an!");
-            locname = document.getElementById("locname").value;
-            locpage = document.getElementById("locpage").value;
-            box     = document.getElementById("gewaehlte_essen");
-            for (var i = 0; i < box.options.length; i++) {
-                locessen[i] = box.options[i].text;
-                // alert ("Name: "+locname+" Page: "+locpage+" Essensarray: "+locessen[i]);
-            }
-
-            $.ajax({
-                type    : "POST",
-                url     : "procedures.php",
-                data    : {callFunction: 'insertLocation', p1: locname, p2: locpage, p3: locessen},
-                dataType: 'text',
-                success : function (data) {
-                }
-            });
-        }
-
-        function essen_anlegen() {
-            essenName = document.getElementById("essenName").value;
-
-            $.ajax({
-                type    : "POST",
-                url     : "procedures.php",
-                data    : {callFunction: 'insertEssen', p1: essenName},
-                dataType: 'text',
-                success : function (data) {
-                    window.location.reload();
-                }
-            });
-
-            // window.location.reload();
-
-        }
-
-        function essen_laden() {
-            box = document.getElementById("verfuegbare_essen");
-
-            $.ajax({
-            type    : "POST",
-            url     : "procedures.php",
-            data    : {callFunction: 'reloadEssen'},
-            dataType: 'text',
-            success : function (data) {
-                essenDropdown = JSON.parse(data);
-
-                for (var i = 0; i < essenDropdown.length; i++) {
-                    element = document.createElement("option");
-                    element.appendChild(document.createTextNode(essenDropdown[i]['name']));
-                    box.appendChild(element);
-                }
-            }
-            });
-
-         }
-
-
-        function essen_zuweisen() {
-            // alert("Ich füge der Location ein Essen hinzu!");
-            referenz  = document.newloc.verfuegbare_essen;
-            meinEssen = referenz.value;
-
-            box   = document.getElementById("gewaehlte_essen");
-            error = false;
-
-            for (var i = 0; i < box.options.length; i++) {
-                if (box.options[i].text == meinEssen) {
-                    error = true;
-                }
-            }
-            if (meinEssen && box.options.length < 5 && error == false) {
-                element = document.createElement("option");
-                element.appendChild(document.createTextNode(meinEssen));
-                box.appendChild(element);
-            }
-            else {
-                alert("Höchstens 5 Essen pro Location, keine doppelten Essen und keine leeren Essen hinzufügen, bitte!!");
-            }
-
-            return false;
-        }
-
-        function essen_entfernen() {
-            box = document.getElementById("gewaehlte_essen");
-            box.remove(box.selectedIndex);
-        }
-
-        function locations_abfragen() {
-            
-            $.ajax({
-                type    : "POST",
-                url     : "procedures.php",
-                data    : {callFunction: 'insertEssen'},
-                dataType: 'text',
-                success : function (data) {
-                    window.location.reload();
-                }
-            });
-
-            // window.location.reload();
-
-        }
-
-        -->
-    </script>
-
+    
 </head>
 
 <body>
@@ -221,7 +106,6 @@ include("includes/includeBody.php");
     </div>
 </div>
 
-<script>essen_laden();</script>
 
 <?php
 include("includes/includeFooter.php");
